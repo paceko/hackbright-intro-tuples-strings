b1745c53a2333e2309ff192e7e84d8f4b56f509e
sandi <sandimac@users.noreply.github.com>
2015-11-30 21:18:44 -0800

    tuple

---
+++ 02-strings/parse_strings.py
@@ -0,0 +1,39 @@
+#my_name = "patricia" 
+
+#print my_name.split()
+#print list(my_name)
+
+#numbers = "1,2,3,4,5"
+
+#print [int(i) for i in numbers.split(",")]  
+
+#fish = "One fish two fish red fish blue fish"
+
+#fish_split = fish.split(" ")
+
+#final_list = []
+
+#for i in fish_split:
+#   if(i != "fish"):
+#        final_list.append(i)
+
+#print final_list
+
+
+#str = "item:apples,quantity:4,price:1.50\n"
+
+#items = ["item:apples,quantity:4,price:1.50\n", "item:pears,quantity:5,price:2.00\n", "item:cereal,quantity:1,price:4.49\n"]
+
+
+#def calculate_bill(items_input):
+#    total = 0
+#    for element in items_input:
+#        split_items = element.split(",")
+#        quantity = float(split_items[1].split(":")[1])
+#        price = float(split_items[2].split(":")[1])
+#        total += quantity * price
+#    return total 
+
+#print calculate_bill(items)
+
+
